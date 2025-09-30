4a40004e0303fb11c7937362f7031bd5ac3291a2
duda <maria.lucien.santos@escola.pr.gov.br>
2025-09-30 07:36:51 -0300

    aula 3009

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo')
 avanca,array.forEach(button => {
     button,addEventListener('click',function () {
  const atual = this.document.querySelector ('.ativo');
-    }
+ const proximoPasso = 'passo-' + this. getAttribut ('data-proximo')  
 
-
-  )
-}
-
-)
+ atual.classList.remove('ativo')
+ document.getElementById(proximoPasso).classList.add('ativo')
+} )
+})
